43c05d9e24e05292b3d3a460646e616f312ac1cf
denil <denilson.dalg@gmail.com>
2018-12-08 00:27:40 -0600

    Modificando

---
+++ login_Register/BackEnd/Conexion.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: denil
+ * Date: 8/12/2018
+ * Time: 12:22 AM
+ */
+
+$mysqli = new mysqli("localhost", "root", "", "usuarios");
+
+
+?>
---
+++ login_Register/BackEnd/Login.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: denil
+ * Date: 8/12/2018
+ * Time: 12:26 AM
+ */
